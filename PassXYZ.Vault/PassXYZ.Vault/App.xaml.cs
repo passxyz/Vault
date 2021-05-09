f815f33f4b92285d29d2e0c0ca5dd1f613b17e0b
Roger Ye <shugaoye@yahoo.com>
2021-05-09 10:07:39 +0800

    Add project files.

---
+++ PassXYZ.Vault/PassXYZ.Vault/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using PassXYZ.Vault.Services;
+using PassXYZ.Vault.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PassXYZ.Vault
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PassXYZ.Vault/PassXYZ.Vault/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using PassXYZ.Vault.ViewModels;
+using PassXYZ.Vault.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace PassXYZ.Vault
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
