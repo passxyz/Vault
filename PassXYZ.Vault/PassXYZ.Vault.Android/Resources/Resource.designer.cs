226ca4113a62f115878a1437b943c6b292598906
Roger Ye <shugaoye@yahoo.com>
2021-07-31 16:32:21 +0800

    Updated logo

--- PassXYZ.Vault/PassXYZ.Vault.Android/Resources/Resource.designer.cs
+++ PassXYZ.Vault/PassXYZ.Vault.Android/Resources/Resource.designer.cs
@@ -11788,16 +11788,19 @@ namespace PassXYZ.Vault.Droid
 			public const int passxyz_logo = 2131165327;
 			
 			// aapt resource value: 0x7F070090
-			public const int test_custom_background = 2131165328;
+			public const int passxyz_wechat = 2131165328;
 			
 			// aapt resource value: 0x7F070091
-			public const int tooltip_frame_dark = 2131165329;
+			public const int test_custom_background = 2131165329;
 			
 			// aapt resource value: 0x7F070092
-			public const int tooltip_frame_light = 2131165330;
+			public const int tooltip_frame_dark = 2131165330;
 			
 			// aapt resource value: 0x7F070093
-			public const int xamarin_logo = 2131165331;
+			public const int tooltip_frame_light = 2131165331;
+			
+			// aapt resource value: 0x7F070094
+			public const int xamarin_logo = 2131165332;
 			
 			static Drawable()
 			{
